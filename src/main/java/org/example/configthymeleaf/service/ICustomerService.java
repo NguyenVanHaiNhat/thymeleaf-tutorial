def05ed427473b1281f4bce073accf26099aa021
nhat0120230 <nhatnguyen22102003@gmail.com>
2024-03-30 23:46:19 +0700

    done

---
+++ src/main/java/org/example/configthymeleaf/service/ICustomerService.java
@@ -0,0 +1,13 @@
+package org.example.configthymeleaf.service;
+
+import org.example.configthymeleaf.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+    void save(Customer customer);
+    Customer findById(int id);
+    void update(int id, Customer customer);
+    void remove(int id);
+}
